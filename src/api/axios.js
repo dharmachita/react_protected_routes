3b360672d4d60150d11ff20ababae068b82ce70c
Mauro Rambo <mauroemmanuel22@gmail.com>
2023-04-07 16:40:15 -0300

    hc ep

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = process.env.ENDPOINT;
+const BASE_URL = 'https://test.servi-confort.site/api/v1';
 
 export default axios.create({
     baseURL: BASE_URL
